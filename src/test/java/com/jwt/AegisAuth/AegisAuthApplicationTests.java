af8a9e03c8970fda7a88256224b0be7cda76c216
Rashi990 <rashinilakshika2020@gmail.com>
2026-05-22 11:43:07 +0530

    Initial setup

---
+++ src/test/java/com/jwt/AegisAuth/AegisAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.AegisAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
