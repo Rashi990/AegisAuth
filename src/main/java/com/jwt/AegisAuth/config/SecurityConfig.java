c1936302cb57e3f53d6eac6625c5e44e197d4ed9
Rashi990 <rashinilakshika2020@gmail.com>
2026-05-26 23:49:40 +0530

    Configure authentication provider and password encoder

--- src/main/java/com/jwt/AegisAuth/config/SecurityConfig.java
+++ src/main/java/com/jwt/AegisAuth/config/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.jwt.AegisAuth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,11 +22,23 @@ public class SecurityConfig {
                 .sessionManagement(s->s.
                         sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(r->r.
-//                        requestMatchers("api/v1/auth/**").permitAll().
-                        requestMatchers("api/v1/auth/login").permitAll().
+//                        requestMatchers("/api/v1/auth/**").permitAll().
+                        requestMatchers("/api/v1/auth/login").permitAll().
                         anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
     }
 }
